7166d15529f1ae8b7c6bebce0a51a5f07fb90e9e
10daniel2001 <cd38668cd@gmail.com>
2026-08-09 15:17:57 -0300

    Iniciando parte esplicativa sobre POO em python

---
+++ notas.py
@@ -0,0 +1,7 @@
+#Oque e uma classe em OOP
+"Classe e o molde do projeto, onde se declara os atributs é os metodos, caracteristicas do objetos"
+
+
+#OQUE E O METODO CONTRUTOR __INIT__
+"O metodo INIT e o construtor, do objeto, nele esta todas as caracteristicas do objetos"
+"INiT e chamado de forma automatica sempre quando cria um novo objetos"
