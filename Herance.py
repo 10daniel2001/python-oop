1548b33e5b45f5b75b05923b7c25824e18681843
10daniel2001 <cd38668cd@gmail.com>
2026-08-15 21:07:31 -0300

    Definindo metodos e atributos da sub class

--- Herance.py
+++ Herance.py
@@ -13,3 +13,22 @@ class Herance:
             print("Vocẽ é menor de idade")
 
 # Sub class
+class Filha(Herance):
+
+    #Aqui onde a Subclass pega todos os mesmo conceitos de atributos da Super class
+    #Onde a herança da class pai passa a copia dos atributos para a filha
+    #Mas tem a opçao de criar proprios atributos para a class filha, como lá esta city
+    def __init__(self, name, age, year, city):
+        super().__init__(name, age, year)
+        self.city = city
+
+    def imprimir(self):
+        print(f"Sua cidade e {self.city}")
+
+def main():
+
+    super = Herance("Carlos", 25, 2026)
+    #Obejto da super class istaciado
+
+    filha = Filha("Daniel", 20, 2021, "Goiania Go")
+    #Objeto da subclass istanciado
