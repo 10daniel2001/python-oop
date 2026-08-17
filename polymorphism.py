be978ba576bed56ca20a66deabd711e080a9bb6f
carlos daniel <cd38668cd@gmail.com>
2026-08-17 18:13:17 -0300

    Implementando metodos

--- polymorphism.py
+++ polymorphism.py
@@ -1,7 +1,16 @@
+"Nossa super class se chama polymorphism"
+
+#Super class
 class Polymorphism:
-    def __init__(self, name, id):
+    def __init__(self, name, id, age):
         self.name = name
         self.id = id
+        self.age = age
+        "Nada esta privado, uma observaçao"
 
+    # Method the super class
     def print_id(self):
         print(f"Your ID is - {self.id}")
+
+    def your_Age(self, age):
+        print(f"Your age is {}")
