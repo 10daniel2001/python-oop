988a7a198331bc3605a3a58b7891294bce2e654e
carlos daniel <cd38668cd@gmail.com>
2026-08-17 18:21:44 -0300

    Implementando partes com super da sub class

--- polymorphism.py
+++ polymorphism.py
@@ -2,15 +2,26 @@
 
 #Super class
 class Polymorphism:
-    def __init__(self, name, id, age):
+    def __init__(self, name, is_id):
         self.name = name
-        self.id = id
-        self.age = age
+        self.is_id = is_id
+
         "Nada esta privado, uma observaçao"
 
     # Method the super class
     def print_id(self):
-        print(f"Your ID is - {self.id}")
-
+        print(f"Your ID is - {self.is_id}")
+    #Method the super class
     def your_Age(self, age):
-        print(f"Your age is {}")
+        print(f"Your age is {age}")
+
+#Sub class
+class Child(Polymorphism):
+    def __init__(self, name, age, ident):
+        # Call the parent class constructor, passing the required arguments
+        super().__init__(name, is_id=ident)
+        self.age = age
+
+
+def main():
+
