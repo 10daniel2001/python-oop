99feb9af6f3030047cbe274fefa5dae5c2859c5f
carlos daniel <cd38668cd@gmail.com>
2026-08-21 21:16:28 -0300

    Metodos comportamentos

--- notas.py
+++ notas.py
@@ -3,10 +3,15 @@
 
 
 #OQUE E O METODO CONTRUTOR __INIT__
-"O método __init__ é o construtor da classe. Ele é chamado"
+"O método __init__ é o constrútor da classe. Ele é chamado"
 "automaticamente toda vez que um objeto é criado, e serve para"
 "inicializar os atributos desse objeto usando o self"
 
 # ATRIBUTOS DE CLASS
 "Atributos da class, sao as caracteristicas formais, de uma class onde o objeto estanciad herda esses atributos"
 "Exemplos como nome, idade, ou id, sao referencias de um objeto"
+
+# METODOS OU COMPORTAMENTOS
+"Metodos sao funçoes da class onde o objeto estanciado desta classe pode usar, ou chamar"
+"Metodos vao definir o tipo de comportamentos que o objeto ter !"
+"Metodos sao ensnciais para o polimorfismo"
